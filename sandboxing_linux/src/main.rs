336862fff4bd1724d962cb5ae1448ac6bd026200
Yves Zoundi <yveszoundi@gmail.com>
2025-04-21 21:20:16 -0400

    Add initial code skeleton for Linux POC

---
+++ sandboxing_linux/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("\nWORK IN PROGRESS {}", "\n");
+    println!("----------------------------------");
+    println!("* Restricting system calls");
+    println!("----------------------------------");
+    println!("- Load a seccomp filter!");
+    println!("- Apply the seccomp filter!");
+
+
+    println!("\n----------------------------------");
+    println!("* Restricting file/device access");
+    println!("----------------------------------");
+    println!("- Load landlock rules!");    
+    println!("- Apply landlock rules!");
+    println!("");
+}
